030b34b6137548ae9334dfe827b9b7bb2ad0a3a1
Daniel Diaz <Daniel Diaz@DESKTOP-D6O6AF7>
2025-03-20 16:09:37 -0500

    Acá vamos a hacer el ejercicio 1.java. Código creado por Daniel

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicio1;
 
-/**
- *
- * @author Daniel Diaz
- */
-public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicio1 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        String perrahp;
     }
     
 }
