2b2e83ad7f459752c08acbd07b0625d48128712a
Danny-16gg" <ccorte22@estudiante.iberoamericana.edu.co>
2025-03-20 16:02:09 -0500

    aca vamos hacer ejercicio 1 hecho por carlos ejercicio1.java

--- ejercicio 1/src/ejercicio/pkg1/Ejercicio1.java
+++ ejercicio 1/src/ejercicio/pkg1/Ejercicio1.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio.pkg1;
 
-/**
- *
- * @author Daniel sexy
- */
 public class Ejercicio1 {
 
     /**
@@ -15,6 +8,7 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola");
     }
     
 }
